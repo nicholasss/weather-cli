d2ebb8040d0f548ae6594b5f4d097faded750c95
nicholasss <nicholaszingleman@gmail.com>
2024-09-18 00:24:18 -0400

    base functionality of retrieving weather data json from endpoint

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import typer
+import requests
+import api
 
 app = typer.Typer()
 
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q=" # city,state,country
+KEY_APPEND = "&appid=" # API KEY
+METRIC_APPEND = "&units=metric"
+
 @app.command()
-def check_weather(city: str):
-	print(f"The weather in {city} currently is 63 degress and rainy.")
+def check_weather(city: str, state: str):
+	country: str = "US" # Hard coding US only
+	
+	requests_url = BASE_URL + city + "," + state + "," + country + KEY_APPEND + api.testing_key + METRIC_APPEND
+
+	r = requests.get(requests_url)
+	print(r.json())
+
 
 if __name__ == "__main__":
 	app()
