1854f3b64592b5172c824cb6fb34069c33b3ec17
nicholasss <nicholaszingleman@gmail.com>
2024-10-13 15:26:18 -0400

    added some logic for processing the json

--- main.py
+++ main.py
@@ -5,19 +5,34 @@ import api
 
 app = typer.Typer()
 
-BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q=" # city,state,country
-KEY_APPEND = "&appid=" # API KEY
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q="
+# city,state,country
+KEY_APPEND = "&appid="  # API KEY
 METRIC_APPEND = "&units=metric"
+
 
 @app.command()
 def check_weather(city: str, state: str):
-	country: str = "US" # Hard coding US only
-	
-	requests_url = BASE_URL + city + "," + state + "," + country + KEY_APPEND + api.testing_key + METRIC_APPEND
+    country: str = "US"  # Hard coding US only
 
-	r = requests.get(requests_url)
-	print(r.json())
+    requests_url = BASE_URL + city + "," + state + "," + \
+        country + KEY_APPEND + api.testing_key + METRIC_APPEND
+
+    weather_response = requests.get(requests_url).json()
+
+    print(weather_response)
+    print(f"Coordinates: {return_coords(weather_response)}")
+    print(f"Current Temp: {return_curr_temp(weather_response)}°C")
+
+
+def return_coords(json_object):
+    coord_object = json_object["coord"]
+    return (coord_object["lat"], coord_object["lon"])
+
+
+def return_curr_temp(json_object):
+    return json_object["main"]["temp"]
 
 
 if __name__ == "__main__":
-	app()
+    app()
