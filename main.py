f502e86bccc9d13777fdb4e888dffa2912d77656
nicholasss <nicholaszingleman@gmail.com>
2024-10-14 18:44:10 -0400

    starting to use rich

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import typer
+from rich.console import Console
 import requests
 import orjson
 import api
 
 app = typer.Typer()
+
+console = Console()
 
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q="
 # city,state,country
@@ -18,11 +21,13 @@ def check_weather(city: str, state: str):
     requests_url = BASE_URL + city + "," + state + "," + \
         country + KEY_APPEND + api.testing_key + METRIC_APPEND
 
-    weather_response = requests.get(requests_url).json()
+    with console.status("Receiving Weather Data...", spinner="dots"):
+        weather_response = requests.get(requests_url).json()
 
-    print(weather_response)
+    console.print_json(data=weather_response)
     print(f"Coordinates: {return_coords(weather_response)}")
     print(f"Current Temp: {return_curr_temp(weather_response)}°C")
+    print(f"Feels Like: {return_feelslike_temp(weather_response)}°C")
 
 
 def return_coords(json_object):
@@ -32,6 +37,10 @@ def return_coords(json_object):
 
 def return_curr_temp(json_object):
     return json_object["main"]["temp"]
+
+
+def return_feelslike_temp(json_object):
+    return json_object["main"]["feels_like"]
 
 
 if __name__ == "__main__":
