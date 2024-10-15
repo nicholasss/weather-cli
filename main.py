101fbc4201afdd2fa57fbbbec55577c64f3dba51
nicholasss <nicholaszingleman@gmail.com>
2024-10-15 16:00:30 -0400

    added in time information and organized the functions into sections

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import typer
 from rich.console import Console
 from rich.padding import Padding
 import requests
+from datetime import datetime as dt
+from datetime import timezone as tz
+from datetime import timedelta
 import orjson
 import api
 
@@ -18,7 +21,6 @@ METRIC_APPEND = "&units=metric"
 @app.command()
 def check_weather(city: str, state: str):
     country: str = "US"  # Hard coding US only
-
     requests_url = BASE_URL + city + "," + state + "," + \
         country + KEY_APPEND + api.testing_key + METRIC_APPEND
 
@@ -29,23 +31,59 @@ def check_weather(city: str, state: str):
 
     console.rule(f"[bold]Current Weather in {weather_response["name"]}")
 
-    console.print(f"Coordinates: {return_coords(
-        weather_response)}")
-    console.print(f"Current Temp: {return_curr_temp(weather_response)}°C")
-    console.print(f"Feels Like: {return_feelslike_temp(weather_response)}°C")
+    console.print(f"Coordinates: {__return_coords(weather_response)}")
+    console.print(f"Current Temp: {__return_curr_temp(weather_response)}°C")
+    console.print(f"Feels Like: {__return_feelslike_temp(weather_response)}°C")
+    console.print(f"Current Time: {__current_local_time(weather_response)}")
+    console.print(f"Local Sunrise Time: {
+                  __return_local_sunrise(weather_response)}")
+    console.print(f"Local Sunset Time: {
+                  __return_local_sunset(weather_response)}")
 
 
-def return_coords(json_object):
+# Location and helper functions
+def __return_coords(json_object):
     coord_object = json_object["coord"]
     return (coord_object["lat"], coord_object["lon"])
 
 
-def return_curr_temp(json_object):
+# Temperature and helper functions
+def __return_curr_temp(json_object):
     return json_object["main"]["temp"]
 
 
-def return_feelslike_temp(json_object):
+def __return_feelslike_temp(json_object):
     return json_object["main"]["feels_like"]
+
+
+# Timezone and helper functions
+def __current_local_time(json_object):
+    current_time = dt.now(tz=tz(timedelta(seconds=json_object["timezone"])))
+    return __time_format(current_time)
+
+
+def __return_local_sunrise(json_object):
+    sunrise_time = __unix_to_utc(json_object["sys"]["sunrise"])
+    return __utc_to_local(sunrise_time, json_object)
+
+
+def __return_local_sunset(json_object):
+    sunset_time = __unix_to_utc(json_object["sys"]["sunset"])
+    return __utc_to_local(sunset_time, json_object)
+
+
+def __utc_to_local(datetime_object, json_object):
+    local_time = datetime_object.astimezone(
+        tz=tz(timedelta(seconds=json_object["timezone"])))
+    return __time_format(local_time)
+
+
+def __unix_to_utc(unix_seconds):
+    return dt.fromtimestamp(unix_seconds, tz.utc)
+
+
+def __time_format(datetime_object):
+    return datetime_object.strftime("%I:%M:%S %p")
 
 
 if __name__ == "__main__":
